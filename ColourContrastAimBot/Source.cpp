91a8d89725b94fda211a925d793eb5ff7cad6c32
x3w0b <x3w0b@ugrad.cs.ubc.ca>
2019-07-13 22:15:17 -0700

    added guard against nametags

--- ColourContrastAimBot/Source.cpp
+++ ColourContrastAimBot/Source.cpp
@@ -6,8 +6,8 @@
 
 using namespace std;
 
-int width = 200;
-int height = 200;
+int width = 160;
+int height = 160;
 
 RGBQUAD * capture(POINT a, POINT b) {
 	// copy screen to bitmap
@@ -39,7 +39,6 @@ RGBQUAD * capture(POINT a, POINT b) {
 	
 	GetDIBits(hDC, hBitmap, 0, height, pixels, (BITMAPINFO*)& bmi, DIB_RGB_COLORS);
 
-
 	// clean up
 	SelectObject(hDC, old_obj);
 	DeleteDC(hDC);
@@ -58,12 +57,13 @@ bool Aim() {
 	RGBQUAD * pixels;
 	POINT targetPos; // centered at top left corner of capture zone
 
+	int sampleCount = 8;
 	double radius, angle;
 	int x, y, index; // centered at center of screen
 	int red, green, blue;
-	int sampleR = 245, sampleG = 160, sampleB = 70; // nc infravision
-	// int sampleR = 180, sampleG = 160, sampleB = 130; // corona ir values
+	// int sampleR = 255, sampleG = 170, sampleB = 80; // nc infravision
 	// int sampleR = 255, sampleG = 145, sampleB = 57; // vs infravision
+	int sampleR = 180, sampleG = 160, sampleB = 130; // corona ir values
 	// int sampleR = 145, sampleG = 145, sampleB = 145; // tr irnv values
 
 	bool sampled = false;
@@ -72,32 +72,31 @@ bool Aim() {
 	while (true) {
 		angle = 2 * 3.141592654 / 8;
 		radius = 2;
-		//0x39
-		if ((GetKeyState(0x39) & 0x100) != 0) { // while key 9 pressed
+		// if ((GetKeyState(0x39) & 0x100) != 0) { // while key 9 pressed
 			// sample colors
 			// cout << "reached" << endl;
-		}
+		// }
 		if ((GetKeyState(VK_RBUTTON) & 0x100) != 0 && !(GetKeyState(VK_SHIFT) & 0x8000)) { // while rmb pressed
 			pixels = capture(a, b);
 			sampled = false;
-
-			for (int i = 0; i < 40000; i++) {
+			for (int i = 0; i < sampleCount * width; i++) {
 				x = (int)(radius * cos(angle) + width / 2);
 				y = (int)(radius * sin(angle) + height / 2);
-				radius = radius + 0.1;
-				angle += 1 / radius;
+				if (i % sampleCount == 0) {
+					radius++;
+				}
+				angle += 2 * 3.141592654 / sampleCount;
 				index = y * width + x;
-
 				if (x < 0 || x > 399 || y < 0 || y > 399) {
-					//cout << "OUT OF BOUNDS  " << x << "  " << y << endl;
 					break;
 				}
-
 				red = (int)pixels[index].rgbRed;
 				green = (int)pixels[index].rgbGreen;
 				blue = (int)pixels[index].rgbBlue;
-				
-				if ((abs(red - sampleR) < 35 && abs(green - sampleG) < 35 && abs(blue - sampleB) < 35) ) {
+				if (red > 250 && green + blue < 10 || green > 250 & red + blue < 10 || blue > 250 & red + green < 10) {
+					break;
+				}
+				if ((abs(red - sampleR) < 30 && abs(green - sampleG) < 30 && abs(blue - sampleB) < 30) || (255 - red < 10 && 255 - green < 10 && 255 - blue < 10) ) {
 					brightest = red + green + blue;
 					targetPos.x = index % width;
 					targetPos.y = index / width;
@@ -114,7 +113,6 @@ bool Aim() {
 					//cout << "BREAK" << endl;
 					break;
 				}
-				
 			}
 			delete[] pixels;
 		}
